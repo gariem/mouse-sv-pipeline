3b6882289fab7f8ab7dd69b2e03260efaa9ba737
Emilio Garcia <egarcia@ebi.ac.uk>
2022-01-21 15:31:40 +0000

    New configuration

--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,8 @@
-PARAMS_FILE="run-params$1.txt"
+DATA_TAG=$1 #Either 'prod' or 'test'
+PARAMS_FILE="run-params-$DATA_TAG.txt"
+NXF_ENV=$2 #Empty or '-q'
+QUIET=$3 #Empty or '-profile lsf'
+
 STRAIN_VALUES="$(cat $PARAMS_FILE | grep 'strains=' | cut -d '=' -f 2 )"
 SURVIVOR_MAX_DIST_VALUES="$(cat $PARAMS_FILE | grep 'survivor.max_dist=' | cut -d '=' -f 2 )"
 SURVIVOR_MIN_SIZE_VALUES="$(cat $PARAMS_FILE | grep 'survivor.min_size=' | cut -d '=' -f 2 )"
@@ -9,78 +13,83 @@ FILTER_VALUES="$(cat $PARAMS_FILE | grep 'filter_hets=' | cut -d '=' -f 2 )"
 
 echo "========== PARAMS =========="
 cat $PARAMS_FILE
-echo "\n"
+echo ""
+echo "DATA_TAG=$DATA_TAG"
+echo "NXF_ENV=$NXF_ENV"
+echo "QUIET=$QUIET"
+echo ""
 echo "============================\n"
+echo ""
 
 echo "Cleaning work, merge, and reports folders"
 rm -rf ./work
 rm -rf ./data/reports
-# rm -rf ./data/merged 
+rm -rf ./data/merged 
 echo "\tDone -> Clean\n"
 
 echo "Collecting metrics From source VCFs"
-nextflow run tasks/metrics_collect.nf \
+nextflow $QUIET run tasks/metrics_collect.nf \
     --intersect_window $INTERSECT_WINDOW_VALUES \
     --filter_hets $FILTER_VALUES \
-    --input_dir ./data/input
+    --input_dir ./data/input $NXF_ENV
 
 rm -rf ./work
-echo "\tDone -> Source Metrics\n"
+echo "  Done -> Source Metrics\n"
 
-# echo "Merging with CombiSV"
-#     nextflow run tasks/merge_combi.nf \
-#         --min_coverage $COMBI_MIN_COVERAGE_VALUES
+echo "Merging with CombiSV"
+    nextflow $QUIET run tasks/merge_combi.nf \
+        --min_coverage $COMBI_MIN_COVERAGE_VALUES $NXF_ENV
 
-# echo "\tDone -> Merge [CombiSV]\n"
+echo "  Done -> Merge [CombiSV]\n"
 
 echo "Collecting metrics for CombiSV"
     nextflow run tasks/metrics_collect.nf \
         --intersect_window $INTERSECT_WINDOW_VALUES \
         --filter_hets $FILTER_VALUES \
-        --input_dir ./data/merged 
+        --input_dir ./data/merged $NXF_ENV
 
 rm -rf ./work
-# echo "\tDone -> Metrics [CombiSV]\n"
+echo "  Done -> Metrics [CombiSV]\n"
 
-# echo "Merging with mapped method"
-# nextflow run tasks/merge_survivor_mapped.nf \
-#     --max_dist $SURVIVOR_MAX_DIST_VALUES \
-#     --min_callers $SURVIVOR_MIN_CALLERS_VALUES \
-#     --min_size $SURVIVOR_MIN_SIZE_VALUES \
-#     --filter_hets $FILTER_VALUES
+echo "Merging with mapped method"
+nextflow $QUIET run tasks/merge_survivor_mapped.nf \
+    --max_dist $SURVIVOR_MAX_DIST_VALUES \
+    --min_callers $SURVIVOR_MIN_CALLERS_VALUES \
+    --min_size $SURVIVOR_MIN_SIZE_VALUES \
+    --filter_hets $FILTER_VALUES $NXF_ENV
 
-# echo " -> Done\n"
+echo "  Done\n"
 
-# echo "Merging with survivor"
-#     nextflow run tasks/merge_survivor_simple.nf \
-#         --max_dist $SURVIVOR_MAX_DIST_VALUES \
-#         --min_callers $SURVIVOR_MIN_CALLERS_VALUES \
-#         --min_size $SURVIVOR_MIN_SIZE_VALUES 
+echo "Merging with survivor"
+    nextflow run tasks/merge_survivor_simple.nf \
+        --max_dist $SURVIVOR_MAX_DIST_VALUES \
+        --min_callers $SURVIVOR_MIN_CALLERS_VALUES \
+        --min_size $SURVIVOR_MIN_SIZE_VALUES $NXF_ENV
 
-# echo " -> Done"
+echo "  Done"
 
 for STRAIN in $(echo $STRAIN_VALUES | sed "s/,/ /g")
 do
     echo "Collecting metrics for merged $STRAIN [Mapped]"
-    nextflow run tasks/metrics_collect.nf \
+    nextflow $QUIET run tasks/metrics_collect.nf \
         --intersect_window $INTERSECT_WINDOW_VALUES \
         --filter_hets 0 \
         --input_dir ./data/merged \
-        --strain "$STRAIN-mapped"
+        --strain "$STRAIN-mapped" $NXF_ENV
 
     rm -rf ./work
-    echo " -> Done ($STRAIN [Mapped])\n"
+    echo "  Done ($STRAIN [Mapped])\n"
     
 
     echo "Collecting metrics for merged $STRAIN [Survivor]"
-    nextflow run tasks/metrics_collect.nf \
+    nextflow $QUIET run tasks/metrics_collect.nf \
         --intersect_window $INTERSECT_WINDOW_VALUES \
         --filter_hets $FILTER_VALUES \
         --input_dir ./data/merged \
-        --strain "$STRAIN-survivor"
+        --strain "$STRAIN-survivor" $NXF_ENV
 
     rm -rf ./work
-    echo " -> Done ($STRAIN [Survivor])\n"
+    echo "  Done ($STRAIN [Survivor])\n"
     
 done
 
